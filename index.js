72f6bf5b71d9779d22a8d1b306db37d603072081
bmansk8 <bmansk8@yahoo.com>
2020-04-20 11:04:06 -0400

    be in touch
    created a simple acme landing page

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/alpine.js', function (req, res) {
+  res.sendFile(path.join(__dirname + '/node_modules/alpinejs/dist/alpine.js'));
+});
+
+app.get('/', (req, res) => res.redirect('index.html'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
